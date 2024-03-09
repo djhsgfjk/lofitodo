3eb7185d5d9fca44606ba81ef072d6871e3532bb
Diana Butiakova <butyakova01@mail.ru>
2024-03-09 22:41:16 +0300

    initialized project

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: process.env.NODE_ENV || 'development',
+    entry: {
+        bundle: path.resolve(__dirname, 'src\/scripts\/index.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'dev'),
+        filename: '[name].js',
+        clean: true,
+    },
+    devtool: process.env.NODE_ENV ? false : 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpg)$/i,
+                type: 'asset/resource',
+                
+            },
+            { test: /\.css$/, use: ['style-loader', 'css-loader']},
+            { test: /\.js$/, use: 'babel-loader' },
+            { 
+                test: /\.svg$/,
+                use: [
+                    {
+                        loader: 'svg-inline-loader' ,
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+
+        new HtmlWebpackPlugin({
+            title: 'LofiTodo',
+            favicon: path.resolve(__dirname, 'src/assets/logo.svg'),
+            filename: 'template.html',
+            template: path.resolve(__dirname, 'src/\template.html'),
+        }),
+    ],
+};
